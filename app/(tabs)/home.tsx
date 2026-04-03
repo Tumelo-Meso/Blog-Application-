23af8079f74129b1017a6fb29e2db9c3fd8869ce
Tumelo Meso <tumelomeso@icloud.com>
2026-04-03 21:52:43 +0200

    Account page user information retrival implemenated partially

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 import { Text, View } from 'react-native';
-
 const Home = () => {
 
-  //Ignore this for now 
-  const basePoint = "http://172.16.6.115:9020"
- 
   return (
     <View>
       <Text>Home</Text>
